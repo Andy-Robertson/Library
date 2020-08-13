d7eba656ed1973170cec21496b24d9d42dbe8bef
Andy-Robertson <andrew.j.robertson@posteo.uk>
2020-08-13 08:25:18 +0100

    constructor refactor and basic  book add function added

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    // the constructor...
 
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
-    this.info = function () {
+}
 
-        return '${this.title} by ${this.author}, ${this.pages}, ${this.read}';
+function addBookToLibrary(title, author, pages, read) {
 
-    }
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    console.log(book);
+    console.log(myLibrary);
 
 }
 
-function addBookToLibrary() {
-
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let pages = document.getElementById('pages').value;
-    let read = document.getElementById('read').value;
-
-}
-
-const book1 = new Book("Gone with the wind", "James May", 234, "not read yet");
+//const book = new Book("Gone with the wind", "James May", 234, "not read yet");
